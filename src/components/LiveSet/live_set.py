64eeb031f9d4c9aeb8681ffcfc02abbf7456cc81
Brent Thayer <bthayer1@gmail.com>
2023-05-02 22:54:19 -0600

    config parser working

--- src/components/LiveSet/live_set.py
+++ src/components/LiveSet/live_set.py
@@ -2,10 +2,11 @@ import os.path
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element
 from xml.dom import minidom
-
+from typing import List
 import yaml
+from dataclasses import dataclass
 
-
+import os
 import sys
 
 sys.path.append(f"{os.getcwd()}/src/components/")
@@ -31,8 +32,6 @@ class LiveSet:
         tracks_tree = ET.ElementTree(tracks_element)
         self.root.append(tracks_tree.getroot())
 
-        
-
     # def populate_live_set(self):
     #     self.add_tracks()
 
@@ -40,32 +39,104 @@ class LiveSet:
         reparsed = minidom.parseString(ET.tostring(self.tree.getroot()).decode("utf8"))
         return reparsed.toprettyxml(indent="\t")
 
+
 # def parse_returns(returns):
 #     return_tracks = []
 #     for return_track in returns:
 #         return_tracks.append(AudioTrack(return_track["name"], return_track["id"], return_track["group_id"], return_track["sends"]))
 #     return return_tracks
 
+
 def parse_tracks(tracks):
-    audio_tracks = []
-    for track in tracks:
-        audio_track = AudioTrack(track["ame"], track["id"], track["group_id"], track["sends"])
-        name, id, group_id, sends
-    return audio_tracks
+    # audio_tracks = []
+    # for track in tracks:
+    #     audio_track = AudioTrack(
+    #         track["ame"], track["id"], track["group_id"], track["sends"]
+    #     )
+    #     name, id, group_id, sends
+    # return audio_tracks
+    pass
 
-def parse_config(config):
-    with open("set-up.yaml", "r") as f:
-        data = yaml.safe_load(f)
 
-    # returns = parse_returns(data["Returns"])
-    tracks = parse_tracks(data["Tracks"])
+@dataclass
+class TracksConfigObject:
+    path: str
+    send: int
 
-    
-    
+
+@dataclass
+class SongConfigObject:
+    id: int
+    name: str
+    path: str
+    cues: str
+    tracks: List[TracksConfigObject]
+
+
+@dataclass
+class SongsConfigObject:
+    cues_path: str
+    tracks_path: str
+    songs: List[SongConfigObject]
+
+
+@dataclass
+class ReturnConfigObject:
+    id: int
+    name: str
+
+
+@dataclass
+class ConfigObject:
+    songs: SongsConfigObject
+    returns: ReturnConfigObject
+
+
+def parse_config(yaml_config):
+    with open(yaml_config, "r") as f:
+        yaml_data = yaml.safe_load(f)
+
+    exclude_keys = ["CuesPath", "TracksPath"]
+    songs = {
+        k: yaml_data["Songs"][k]
+        for k in set(list(yaml_data["Songs"].keys())) - set(exclude_keys)
+    }
+    songs_list = []
+    for song_num, song_info in songs.items():
+        tracks_list = []
+        for path, send in song_info["Tracks"].items():
+            tracks_list.append(TracksConfigObject(path=path, send=send))
+
+        song_config = SongConfigObject(
+            id=song_num,
+            name=song_info["Name"],
+            path=song_info["Path"],
+            cues=song_info["Cues"],
+            tracks=tracks_list,
+        )
+        songs_list.append(song_config)
+
+    song_config = SongsConfigObject(
+        cues_path=yaml_data["Songs"]["CuesPath"],
+        tracks_path=yaml_data["Songs"]["TracksPath"],
+        songs=songs_list,
+    )
+
+    return_list = []
+    for id, name in yaml_data["Returns"].items():
+        return_config = ReturnConfigObject(id=id, name=name)
+        return_list.append(return_config)
+
+    return ConfigObject(songs=song_config, returns=return_list)
 
 
 if __name__ == "__main__":
     config = parse_config("set-up.yaml")
+
+    print(config.songs.songs[0].tracks[0].path)
+
+    # print(config.songs)
+    # print(config.returns)
 
     # tracks = [
     #     AudioTrack("test1", 1, 1, 1),
@@ -73,7 +144,7 @@ if __name__ == "__main__":
     # ]
     # live_set = LiveSet(tracks, None, None)
     # print(live_set.ET_to_string())
-    
+
     # print(live_set)
 
     # print(live_set.tracks[0].ET_to_string())
---
+++ src/utils/config_parser.py
@@ -0,0 +1,105 @@
+from typing import List
+import yaml
+from dataclasses import dataclass
+
+
+@dataclass
+class TracksConfigObject:
+    path: str
+    send: int
+
+
+@dataclass
+class SongConfigObject:
+    id: int
+    name: str
+    path: str
+    cues: str
+    tracks: List[TracksConfigObject]
+
+
+@dataclass
+class SongsConfigObject:
+    cues_path: str
+    tracks_path: str
+    songs: List[SongConfigObject]
+
+
+@dataclass
+class ReturnConfigObject:
+    id: int
+    name: str
+
+
+@dataclass
+class ConfigObject:
+    songs: SongsConfigObject
+    returns: ReturnConfigObject
+
+
+class YamlConfigParser:
+    def __init__(self):
+        self.exclude_keys = ["CuesPath", "TracksPath"]
+
+    def load_yaml(self, yaml_config):
+        with open(yaml_config, "r") as f:
+            yaml_data = yaml.safe_load(f)
+        self.yaml_data = yaml_data
+
+    def parse_songs(self):
+        self.songs = {
+            k: self.yaml_data["Songs"][k]
+            for k in set(list(self.yaml_data["Songs"].keys())) - set(self.exclude_keys)
+        }
+
+    def set_tracks_list(self, song_info):
+        tracks_list = []
+        for path, send in song_info["Tracks"].items():
+            tracks_list.append(TracksConfigObject(path=path, send=send))
+        return tracks_list
+
+    def set_songs_list(self):
+        self.songs_list = []
+        for song_num, song_info in self.songs.items():
+            tracks_list = self.set_tracks_list(song_info)
+            song_config = SongConfigObject(
+                id=song_num,
+                name=song_info["Name"],
+                path=song_info["Path"],
+                cues=song_info["Cues"],
+                tracks=tracks_list,
+            )
+            self.songs_list.append(song_config)
+
+    def set_songs_config(self):
+        self.songs_config = SongsConfigObject(
+            cues_path=self.yaml_data["Songs"]["CuesPath"],
+            tracks_path=self.yaml_data["Songs"]["TracksPath"],
+            songs=self.songs_list,
+        )
+
+    def set_returns_list(self):
+        self.return_list = []
+        for id, name in self.yaml_data["Returns"].items():
+            return_config = ReturnConfigObject(id=id, name=name)
+            self.return_list.append(return_config)
+
+    def set_config(self):
+        self.config = ConfigObject(songs=self.songs_config, returns=self.return_list)
+
+    def parse(self, yaml_config):
+        self.load_yaml(yaml_config)
+        self.parse_songs()
+        self.set_songs_list()
+        self.set_songs_config()
+        self.set_returns_list()
+        self.set_config()
+        return self.config
+
+
+if __name__ == "__main__":
+    parser = YamlConfigParser()
+    config = parser.parse("set-up.yaml")
+    print(config.returns)
+
+    # print(config.songs.songs[0].tracks[0].path)
